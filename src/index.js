a2eebab18fecd44097a1114db34f385a252a22f9
amkumar712 <amkumar712@gmail.com>
2017-07-30 23:38:43 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+//AIzaSyBvsQ3LXF-rgtMYBvRg_h25OxqQZzHsOuk
+
+const App = () => {
+	return (
+			<h1>
+				Hi!!
+			</h1>
+		);
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
